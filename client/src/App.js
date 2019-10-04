62175924aafb9ed92eca46e10ea28649f20381c7
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2019-10-04 08:59:32 +0100

    implemented 3custom hooks; Stretch attempted

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,44 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Players from "./components/Players";
+import Navbar from "./components/Navbar";
+
+const useLocalStorage = (key, initialValue) =>{
+  const [storedValue, setStoredValue] = useState(() => {
+    const item = window.localStorage.getItem(key);
+    return item ? JSON.parse(item) : initialValue;
+  });
+
+  const setValue = value => {
+    // Save state
+    setStoredValue(value);
+    // Save to local storage
+    window.localStorage.setItem(key, JSON.stringify(value));
+  };
+  return [storedValue, setValue];
+}
+
+export const useDarkMode = (initialValue) => {
+  const [someValue, setSomeValue] = useLocalStorage('Dark Theme', initialValue);
+
+  useEffect(() => {
+    const bodyElement = document.querySelector('body');
+    if(someValue){
+      bodyElement.classList.add('dark-mode');
+    }else{
+      bodyElement.classList.remove('dark-mode');
+    }
+  },[someValue]);
+
+  return [ someValue, setSomeValue]
+}
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <Navbar/>
         <h1>TABLE:</h1>
         <Players/>
       </div>
