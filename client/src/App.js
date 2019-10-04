39477f99e18f947766174e61ca7e1c0b6bdbd76e
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2019-10-04 09:08:44 +0100

    refactored component

--- client/src/App.js
+++ client/src/App.js
@@ -4,35 +4,9 @@ import "./App.css";
 import Players from "./components/Players";
 import Navbar from "./components/Navbar";
 
-const useLocalStorage = (key, initialValue) =>{
-  const [storedValue, setStoredValue] = useState(() => {
-    const item = window.localStorage.getItem(key);
-    return item ? JSON.parse(item) : initialValue;
-  });
 
-  const setValue = value => {
-    // Save state
-    setStoredValue(value);
-    // Save to local storage
-    window.localStorage.setItem(key, JSON.stringify(value));
-  };
-  return [storedValue, setValue];
-}
 
-export const useDarkMode = (initialValue) => {
-  const [someValue, setSomeValue] = useLocalStorage('Dark Theme', initialValue);
 
-  useEffect(() => {
-    const bodyElement = document.querySelector('body');
-    if(someValue){
-      bodyElement.classList.add('dark-mode');
-    }else{
-      bodyElement.classList.remove('dark-mode');
-    }
-  },[someValue]);
-
-  return [ someValue, setSomeValue]
-}
 
 class App extends React.Component {
   render() {
