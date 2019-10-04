82bfdfbd5b9db74a861ef7b3ef23554de5591d37
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2019-10-04 14:39:28 +0100

    corrections: rempved white space

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,17 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import cup from "./cup.png";
 import "./App.css";
 import Players from "./components/Players";
 import Navbar from "./components/Navbar";
 
-
-
-
-
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <Navbar/>
-        <img  src={cup}/>
+        <Navbar />
+        <img src={cup} />
         <h1>Statistics:</h1>
-        <table>
-      <tbody>
-
-      <tr>
-      <th>S/N</th>
-        <th>Name</th>
-        <th>Country</th>
-        <th>Searches</th>
-        
-      </tr>
-      </tbody>
-    </table>
-        <Players/>
+        <Players />
       </div>
     );
   }
